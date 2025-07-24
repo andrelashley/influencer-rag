df22fb8d0ce9ad647952e5c1a8eaa3a95ca48dee
André Lashley <andre.lashley@gmail.com>
2025-07-23 20:45:18 -0700

    Add a templates directory

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Andre'}
+    pdfs = [
+        {
+            'owner': {'username': 'John'},
+            'title': 'info.pdf'
+        },
+        {
+            'owner': {'username': 'James'},
+            'title': 'sample.pdf'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, pdfs=pdfs)
